8e475521f285d80e2a03321d53d86a9023ed72c2
Asus <Asus@Asus-PC>
2018-05-18 14:36:28 +0200

    fine lezione 25

--- Flight/src/it/polito/tdp/flight/model/Airport.java
+++ Flight/src/it/polito/tdp/flight/model/Airport.java
@@ -1,5 +1,8 @@
 package it.polito.tdp.flight.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Airport {
 
 	private int airportId;
@@ -13,6 +16,7 @@ public class Airport {
 	private float timezone;
 	private String dst;
 	private String tz;
+	private List<Route> routes;
 
 	public Airport(int airportId, String name, String city, String country, String iataFaa, String icao,
 			double latitude, double longitude, float timezone, String dst, String tz) {
@@ -28,6 +32,7 @@ public class Airport {
 		this.timezone = timezone;
 		this.dst = dst;
 		this.tz = tz;
+		this.routes=new ArrayList<Route>();
 	}
 
 	public int getAirportId() {
--- Flight/src/it/polito/tdp/flight/model/Model.java
+++ Flight/src/it/polito/tdp/flight/model/Model.java
@@ -1,5 +1,37 @@
 package it.polito.tdp.flight.model;
 
-public class Model {
+import java.util.ArrayList;
+import java.util.List;
 
+import it.polito.tdp.flight.db.FlightDAO;
+
+public class Model {
+	FlightDAO fdao=null;
+	List<Airport> airports;
+	List<Airline> airlines;
+	List<Route> routes;
+	
+	AirlineIdMap airlineIdMap;
+	AirportIdMap airportIdMap;
+	RouteIdMap routeIdMap;
+	
+	public Model() {
+		fdao=new FlightDAO();
+		airlineIdMap=new AirlineIdMap();
+		airportIdMap=new AirportIdMap();
+		routeIdMap=new RouteIdMap();
+		
+//		airports=new ArrayList<Airport>();
+//		airlines=new ArrayList<Airline>();
+
+		List<Airline> airlines = fdao.getAllAirlines(airlineIdMap);
+		System.out.println(airlines);
+
+		List<Airport> airports = fdao.getAllAirlines(airportIdMap);
+		System.out.println(airports);
+
+		List<Route> routes = fdao.getAllRoutes(airlineIdMap,airportIdMap,routeIdMap);;
+		System.out.println(routes);
+		
+	}
 }
